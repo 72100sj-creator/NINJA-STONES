53b39c39ea36f7d37d6289e3a8b0e32d559ff0ec
Seb <72100sj@gmail.com>
2026-07-11 14:57:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker basique pour rendre l'application PWA fonctionnelle
-const CACHE_NAME = 'ninja-stones-v8';
+const CACHE_NAME = 'ninja-stones-v9';
 const urlsToCache = [
   './',
   './index.html',
